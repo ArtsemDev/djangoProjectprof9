5c45479ba869de980d5f07fdc71b1ed2c0bb8dc4
milvus <thommy.python.dev@gmail.com>
2022-12-12 21:03:04 +0300

    initial

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils.timezone import now
 
 
@@ -56,6 +57,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog_post', kwargs={'post_slug': self.slug})
 
     @property
     def date(self) -> str:

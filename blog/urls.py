fd0a39b5163e5a4a0980ecacb9ee163bac514c4a
milvus <thommy.python.dev@gmail.com>
2022-12-19 20:11:53 +0300

    class based view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import about, contact, PostListView, PostDetailView
+from .views import ContactCreateView, PostListView, PostDetailView, AboutTemplateView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog_posts'),
-    path('contact/', contact, name='blog_contact'),
-    path('about/', about, name='blog_about'),
+    path('contact/', ContactCreateView.as_view(), name='blog_contact'),
+    path('about/', AboutTemplateView.as_view(), name='blog_about'),
     path('<slug:post_slug>/', PostDetailView.as_view(), name='blog_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, TemplateView
 
-from .models import Post
+from .models import Post, Contact
 from .forms import ContactForm
 
 
@@ -42,6 +43,42 @@ class PostDetailView(BaseMixin, DetailView):
         return context
 
 
+class AboutTemplateView(BaseMixin, TemplateView):
+    template_name = 'blog/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update(self.context)
+        context['heading'] = 'about us'
+        context['coordinate'] = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10221.793301433216!2d27.54570734394003!3d53.906860922347065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcf94b327141f%3A0xd74e660de1f79fe9!2z0J3QtdC80LjQs9Cw!5e1!3m2!1sru!2sby!4v1671128718358!5m2!1sru!2sby'
+        context['about'] = '''
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+ABOUT ABOUT ABOUT ABOUT        
+'''
+        return context
+
+
+class ContactCreateView(BaseMixin, CreateView):
+    template_name = 'blog/contact.html'
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy('blog_posts')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update(self.context)
+        context['heading'] = 'CONTACT'
+        return context
+
+
 # @require_GET
 # def blog_list(request: HttpRequest):
 #     posts_list = Post.objects.all()
@@ -53,17 +90,17 @@ class PostDetailView(BaseMixin, DetailView):
 #     return render(request, 'blog/post.html', {'post': post})
 
 
-def contact(request: HttpRequest):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-    form = ContactForm()
-    return render(request, 'blog/contact.html', {'contact_form': form})
+# def contact(request: HttpRequest):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#     form = ContactForm()
+#     return render(request, 'blog/contact.html', {'contact_form': form})
 
 
-def about(request: HttpRequest):
-    return render(request, 'blog/about.html')
+# def about(request: HttpRequest):
+#     return render(request, 'blog/about.html')
 
 
 def error404(request, exception):
